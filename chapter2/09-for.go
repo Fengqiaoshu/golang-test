3781c1dce4c43c345e11bb34b65d72a434ce3376
WalnutTTTT <337514245@qq.com>
2022-12-31 23:39:19 +0800

    second

---
+++ chapter2/09-for.go
@@ -0,0 +1,31 @@
+package main
+
+import (
+	"fmt"
+	"time"
+)
+
+func main() {
+
+	i, sum := 0, 0
+	// 第一种
+	for i = 1; i <= 100; i++ {
+		sum += i
+	}
+	fmt.Println(sum)
+	// 第二种
+	i = 1
+	sum = 0
+	for i <= 100 {
+		sum += i
+		i++
+	}
+	fmt.Println(sum)
+	// 每个1s 打印一个haha
+	for {
+		fmt.Println("haha")
+		// 睡眠1秒
+		time.Sleep(time.Second * 1)
+
+	}
+}
